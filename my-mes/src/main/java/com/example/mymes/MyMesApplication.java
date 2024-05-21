fdd6e43b5efb4194a804102bbb793cd3b8562d0b
JyHwa0702 <livm211@daum.net>
2024-05-22 01:14:53 +0900

    gitIgnore 설정

---
+++ my-mes/src/main/java/com/example/mymes/MyMesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMesApplication.class, args);
+	}
+
+}
---
+++ my-mes/src/test/java/com/example/mymes/MyMesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
